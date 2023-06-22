81f41e8042415d4a1d42ffbcb2c788e5a80b7400
AJosueBN <anthonyjosue.bn@gmail.com>
2023-06-22 14:13:35 +0000

    Generated files of what is needed

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+// Import our modular routers for /api through /notes
+const notesRouter = require('./notes');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+
+
+
+module.exports = app;
